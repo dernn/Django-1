a69a162886af4f26286f316cb5e04724d909d337
dernn <fajj@mail.ru>
2024-03-22 12:56:32 +0600

    search_fields, action functions

--- simpleapp/admin.py
+++ simpleapp/admin.py
@@ -2,11 +2,26 @@ from django.contrib import admin
 from .models import Category, Product
 
 
+# функция обнуления товара на складе
+def nullify_quantity(modeladmin,
+                     # request — объект хранящий информацию о запросе
+                     request,
+                     # queryset — набор выделяемых объектов.
+                     queryset):
+    queryset.update(quantity=0)
+
+
+# описание для более понятного представления в админ панели
+nullify_quantity.short_description = 'Nullify products quantity'
+
+
 # создаём новый класс для представления товаров в админке
 class ProductAdmin(admin.ModelAdmin):
     # list_display — это список или кортеж со всеми полями, которые будут в таблице с товарами
     list_display = ('name', 'price', 'on_stock')  # оставляем только имя, цену и свойство 'on_stock'
     list_filter = ('price', 'quantity', 'name')  # простой фильтр для админки
+    search_fields = ('name', 'category__name')  # поиск по имени и категории
+    actions = [nullify_quantity]  # добавляем действия в список
 
 
 # регистрируем модели (подтягиваем в админку)

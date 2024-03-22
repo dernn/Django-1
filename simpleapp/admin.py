812fcc2806526613beeabd07cc93166339809cbc
dernn <fajj@mail.ru>
2024-03-22 10:59:39 +0600

    unregistering the model

--- simpleapp/admin.py
+++ simpleapp/admin.py
@@ -4,3 +4,4 @@ from .models import Category, Product
 # регистрируем модели (подтягиваем в админку)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.unregister(Product)  # разрегистрируем наши товары

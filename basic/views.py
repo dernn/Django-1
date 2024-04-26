2cfa079d1c1c5b7e0601f0107123c98576eb6685
dernn <fajj@mail.ru>
2024-04-26 15:13:07 +0600

    Selecting the current time zone [D17.5]

--- basic/views.py
+++ basic/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from django.views import View
 from basic.models import MyModel
 
+from django.utils import timezone
+from django.shortcuts import redirect
+
+import pytz  # импортируем стандартный модуль для работы с часовыми поясами
+
 
 class Index(View):
     def get(self, request):
@@ -13,6 +18,13 @@ class Index(View):
             # передаём всё объекты модели в контекст
             'models': models,
             'available_languages': ['en', 'ru'],
+            'current_time': timezone.now(),
+            'timezones': pytz.common_timezones  # добавляем все доступные часовые пояса
         }
 
         return HttpResponse(render(request, 'basic/index.html', context))
+
+    #  по пост-запросу будем добавлять в сессию часовой пояс, который и будет обрабатываться кастомным middleware
+    def post(self, request):
+        request.session['django_timezone'] = request.POST['timezone']
+        return redirect('/')
--- project/settings.py
+++ project/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
+    'basic.middlewares.TimezoneMiddleware',  # add custom timezone middleware!
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     # Site cache off [D11.3]
 ]
